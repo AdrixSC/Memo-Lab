a2d288451d34c571e1c3db93973c287bb5ffca75
AdrixSC <adriana.be@outlook.com>
2017-11-30 12:50:08 -0600

    cambios en el css y js, aún falta terminar el proyecto

--- js/app.js
+++ js/app.js
@@ -6,14 +6,24 @@ for (var i = 0; i < cards.length; i++) {
 //console.log([i]);
   item.classList.add('efects') // con add agregamos una classList a cada uno de los div de cards
 //console.log([i]);
-  item.addEventListener('mouseover', rotation);
+  item.addEventListener('click', rotation);
+}
+//Definiendo contadores para las cartas volteadas y los pares
+var flipped = 0;
+var pairs = 0;
+
+function rotation (){ //validamos que la carta se pueda voltear (caso para ocultar las cartas)
+  if (!this.classList.contains ('disabled')) { //si esta clase no contiene la clase disabled (clase imaginaria que nos sirve para hacer referencia de que se lo tenemos que quitar o poner)
+    if(this.classList.contains ('rotation-efect'))}
+    /*Agreamos el atributo selected para interactuar con los estilos, en caso de que no exista el atributo, se fija en true*/
+  this.setAttribute("selected", !(this.getAttribute("selected")==="true"));
+  this.classList.remove("rotation-efect");
+  flipped--;
 }
 
-function rotation (){ // 
-  if (this.classList.contains ('rotation-efect')) {
-    this.classList.remove ('rotation-efect');
-  }
-  else {
-    this.classList.add('rotation-efect');
-  }
+function hasEqualsClasses(firstElement, secondElement){
+  var firstElementClasses = firstElement.classList.value.split(" "); //devuelve un arreglo de strings con todas las clases
+  var secondElementClasses = secondElement.classList.value.split(" ");
+  if (firstElementClasses.length == secondElementClasses.length)
+
 }
